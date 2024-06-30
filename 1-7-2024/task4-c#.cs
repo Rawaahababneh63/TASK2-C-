e5046cc9c05661476ca728bdd06185232a283820
Rawaahababneh63 <rawaahabedelrahamn@gmail.com>
2024-07-01 01:43:57 +0300

    c-sharp1-7

---
+++ 1-7-2024/task4-c#.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    // double result =Convert.ToDouble(task4.summationAndAvg());
+            var result = Operation.SummationAndAvg();
+            Console.WriteLine($"The sum of 10 no is : {result.sum}");
+            Console.WriteLine($"The averge of 10 no is : {result.avg}");
+
+           Operation.CubicNum();
+            Console.ReadLine();
+
+
+        }
+    }
+}
